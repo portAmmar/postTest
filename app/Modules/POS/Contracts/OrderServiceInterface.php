10d9754a380e6ff2217b0e15ae932b3526b09e14
portAmmar <ammar.k.alhussary@proton.me>
2026-07-22 18:29:46 +0300

    fix

--- app/Modules/POS/Contracts/OrderServiceInterface.php
+++ app/Modules/POS/Contracts/OrderServiceInterface.php
@@ -1,11 +1,12 @@
 <?php 
-
 namespace App\Modules\POS\Contracts;
+
+use App\Modules\POS\DTOs\CreateOrderDTO;
+use App\Modules\POS\Models\Order;
+use Illuminate\Database\Eloquent\Collection;
 
 interface OrderServiceInterface
 {
-    public function createOrder(array $orderData): array;
-
-    public function getAllOrders(): array;
-
+    public function createOrder(CreateOrderDTO $dto): Order;
+    public function getAllOrders(): Collection;
 }
